933bc6c73a03733feb13b1f3a2dab4ceb6caa15c
hungdev1831998 <hungdev1831998@gmail.com>
2019-12-24 14:08:53 +0700

    add frontt ui manage employ

--- Food_Order_Manager/Food_Order_Manager/Food_Order_Manager/Program.cs
+++ Food_Order_Manager/Food_Order_Manager/Food_Order_Manager/Program.cs
@@ -15,7 +15,7 @@ namespace Food_Order_Manager
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Menu_Form());
+            Application.Run(new ManageEmploy_Form());
         }
     }
 }
